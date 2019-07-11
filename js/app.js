816205ac2a2f37ca73fac102e881800b71b33b6a
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-07-11 12:05:06 -0700

    feat: Add collision detection method the the Enemy object prototype

--- js/app.js
+++ js/app.js
@@ -6,10 +6,29 @@ var Enemy = function(x, y, speed) {
     this.y = y;
     this.speed = speed;
 
+    // built in variables that represent the width and height of the enemy bugs
+    this.width = 100;
+    this.height = 50;
+
+    //bounding box transform to match bug base in sprite image
+    this.vertTransform = 138-this.height;
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
+// check for collisions by comparing the bounding box of this enemy bug to
+// that of the player - resets the game if a collision is detected!
+Enemy.prototype.checkCollisions = function () {
+    if (player.x + player.horizTransform < this.x + this.width  &&
+        player.x + player.width + player.horizTransform > this.x &&
+		player.y + player.vertTransform < this.y + this.height + this.vertTransform &&
+        player.y + player.height + player.vertTransform > this.y + this.vertTransform) {
+        // The objects are touching, so there is a collision! Reset the game!
+        resetGame()
+}
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -17,7 +36,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + this.speed*(dt)
+    this.x = this.x + this.speed*(dt);
+    // checks collision with player after movement
+    this.checkCollisions();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -55,24 +76,39 @@ var Player = function(x, y) {
     this.x = x;
     this.y = y;
 
+    // built-in bounding box size
+    this.width = 33;
+    this.height = 30;
+
+    //bounding box transform from sprite corner to base of character
+    this.horizTransform = 68 - this.width;
+    this.vertTransform = 138 - this.height;
+
     // The image/sprite for a player character
     this.sprite = 'images/char-boy.png';
 };
 
+// Player method used to move the player character in the game.
+// Triggers a "win" screen upon reaching the water, and forces the player
+// character to remain on the pavers or grass due to bounding conditions
 Player.prototype.handleInput = function(e) {
     movement_mod_x = 100;
     movement_mod_y = 83;
     switch(e) {
+        // character moves left unless at the left edge of the screen
         case 'left':
             if(this.x>0) {
                 this.x = this.x-movement_mod_x;
             }
             break;
+        // character moves right unless at the right edge of the screen
         case 'right':
             if(this.x<400) {
                 this.x = this.x+movement_mod_x;
             }
             break;
+        // character moves up unless directly under the water, in which case
+        // the "win" screen is triggered and the game is reset!
         case 'up':
             if(this.y>90) {
                 this.y = this.y-movement_mod_y;
@@ -81,6 +117,7 @@ Player.prototype.handleInput = function(e) {
                 gameOver();
             }
             break;
+        // character moves down unless at the bottom edge of the screen
         case 'down':
         if(this.y<375) {
             this.y = this.y+movement_mod_y;
@@ -100,6 +137,8 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// helper function used to produce a random number in a range between min and
+// max, inclusive - used to generate enemy bug positions and speeds!
 function randomRange(min, max) {
     let anInt = Math.floor(Math.random() * (max-min+1))+min
     return anInt
@@ -144,16 +183,18 @@ function removePassedEnemies() {
 }
 
 // Place all enemy objects in an array called allEnemies
-// let thisEnemy = new Enemy(-80, 10, 80)
 let allEnemies = populateInitialEnemies();
 
 
 // Continue to generate enemies across the board
-// Also remove enemies that are offscreen to the right
+// Also remove enemies that are offscreen to the right (cannot interact with
+// the player anymore)
 setInterval(function() {
     allEnemies.push(makeBugEnemy());
     removePassedEnemies();
 }, 1500);
+
+
 // Place the player object in a variable called player
 let player = new Player(200, 375);
 
